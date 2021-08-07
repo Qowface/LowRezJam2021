2d219535d77a95efeca2697e2284b7823fdd9572
Robert Marquess <qowsrule@gmail.com>
2021-08-06 21:07:18 -0400

    Set up resolution scaling and draw some sprites.

--- LowRezJam2021/Game1.cs
+++ LowRezJam2021/Game1.cs
@@ -6,8 +6,16 @@ namespace LowRezJam2021
 {
     public class Game1 : Game
     {
+        public const int GameWidth = 64;
+        public const int GameHeight = 64;
+        public const int GameScale = 8;
+        
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private RenderTarget2D _nativeRenderTarget;
+        private Rectangle _actualScreenRectangle;
+
+        Texture2D[] numTextures;
 
         public Game1()
         {
@@ -18,7 +26,11 @@ namespace LowRezJam2021
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _nativeRenderTarget = new RenderTarget2D(GraphicsDevice, GameWidth, GameHeight);
+            _actualScreenRectangle = new Rectangle(x: 0, y: 0, width: GameWidth * GameScale, height: GameHeight * GameScale);
+            _graphics.PreferredBackBufferWidth = GameWidth * GameScale;
+            _graphics.PreferredBackBufferHeight = GameHeight * GameScale;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -27,7 +39,11 @@ namespace LowRezJam2021
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            numTextures = new Texture2D[4];
+            numTextures[0] = Content.Load<Texture2D>("0");
+            numTextures[1] = Content.Load<Texture2D>("1");
+            numTextures[2] = Content.Load<Texture2D>("2");
+            numTextures[3] = Content.Load<Texture2D>("3");
         }
 
         protected override void Update(GameTime gameTime)
@@ -42,9 +58,20 @@ namespace LowRezJam2021
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.SetRenderTarget(_nativeRenderTarget);
+            GraphicsDevice.Clear(Color.Transparent);
+            _spriteBatch.Begin();
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Draw(numTextures[0], new Vector2(2, 2), Color.White);
+            _spriteBatch.Draw(numTextures[1], new Vector2(12, 12), Color.White);
+            _spriteBatch.Draw(numTextures[2], new Vector2(22, 22), Color.White);
+            _spriteBatch.Draw(numTextures[3], new Vector2(32, 32), Color.White);
+
+            _spriteBatch.End();
+            GraphicsDevice.SetRenderTarget(null);
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            _spriteBatch.Draw(_nativeRenderTarget, _actualScreenRectangle, Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
